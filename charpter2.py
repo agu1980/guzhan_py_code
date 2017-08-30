9137519fa96bb1a391524b1e9f8e60a862b93e40
agu1980 <albert.gz@hotmail.com>
2017-08-30 20:37:51 +0800

    charpter2
    charpter2

---
+++ charpter2.py
@@ -0,0 +1,59 @@
+import json
+from collections import defaultdict
+
+# obj1= {1:'a'}
+# obj2= {2:'b'}
+# obj3= {3:'c'}
+# obj4= {4:'d'}
+
+obj1= ['a',8]
+obj2= ['b',42]
+obj3= ['c',3]
+obj4= ['d',6]
+
+test = [obj2,obj1,obj4,obj3]
+test.sort(test,key=lambda test:test[1])
+#resutl = test[2][0]
+
+def get_counts(obj,key):
+    counts = {}
+    for x in obj:
+        if x[str(key)] in counts:
+            counts[x[key]] += 1
+        else:
+            counts[x[key]] = 1
+    return counts
+
+
+def get_counts2(obj,key):
+    counts = defaultdict(int)
+    for x in obj:
+        counts[x[key]] = counts[x[key]] + 1
+    return counts
+
+
+def order(obj, direction, return_number):
+    count_pairs = [['adfa',123],['adfa11',123]]
+    obj1 = [['a', 2], ['b', 2]]
+    if direction == 'ASC':
+        for x in obj.iteritems():
+            y = list(x)
+            count_pairs.append(y)
+    count_pairs.sort()
+    return count_pairs[-return_number:]
+
+
+path = 'C:\Users\guzhan\Documents\GitHub\pydata-book\ch02\usagov_bitly_data2012-03-16-1331923249.txt'
+lines = open(path).readlines()
+records = [json.loads(line) for line in lines]
+for rec in records:
+    if 'tz' in rec:
+        pass
+    else:
+        records.remove(rec)
+
+dict_count = get_counts(records,'tz')
+#dict_count2 = get_counts2(records,'tz')
+top_count = order(dict_count,'ASC', 10)
+for i in top_count:
+    print '{',i,'=>',top_count[i],'}'
